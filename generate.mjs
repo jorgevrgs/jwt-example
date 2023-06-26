e24f5ccab03768eb48e8850f0272d2068d2252aa
Jorge Vargas <jorge@onesafe.io>
2023-06-26 15:11:13 -0500

    feat: receive a kid arg

--- generate.mjs
+++ generate.mjs
@@ -1,11 +1,22 @@
 import { writeFile } from 'fs/promises';
-import { exportJWK, generateKeyPair, exportPKCS8, exportSPKI } from 'jose';
-import { v4 as uuidv4 } from 'uuid';
+import { exportJWK, exportPKCS8, exportSPKI, generateKeyPair } from 'jose';
+import { v4 as uuidv4 } from 'uuid';
+import yargs from 'yargs';
+
+const argv = yargs(process.argv.slice(2))
+  .option('kid', {
+    describe: 'User kid',
+    type: 'string',
+  })
+  .help().argv;
+
+// Receive and kid or generate a random Key ID (kid)
+const kid = argv.kid || uuidv4();
 
 // Generate a new key pair
 const { privateKey, publicKey } = await generateKeyPair('RS256', {
   modulusLength: 2048,
-  publicKeyUse: 'sig'
+  publicKeyUse: 'sig',
 });
 
 // Convert keys to PEM format
@@ -19,9 +30,6 @@ await writeFile('public.pem', publicKeyPEM);
 // Create key object for key.json
 const keyObject = await exportJWK(publicKey, { alg: 'RS256', use: 'sig' });
 
-// Generate a random Key ID (kid)
-const kid = uuidv4();
-
 // Set the kid in the key object
 keyObject.kid = kid;
 keyObject.alg = 'RS256';
@@ -32,10 +40,9 @@ console.log('public.pem', publicKeyPEM);
 console.log('kid', keyObject.kid);
 
 // Create key JSON object
-const keyJSON = { keys: [ keyObject ] };
+const keyJSON = { keys: [keyObject] };
 
 // Save key JSON to key.json file
 await writeFile('key.json', JSON.stringify(keyJSON, null, 2));
 
 console.log('Key pair generated and saved successfully.');
-
